14ce1eb8d6f8b782c6b73461f11fd5ef26fd1599
Robert <zykon@pm.me>
2024-09-30 23:33:07 +0300

    disabled auto-update for lazy and treesitter

--- init.lua
+++ init.lua
@@ -148,12 +148,15 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-commentary',
   'tpope/vim-surround',
   'tpope/vim-fugitive',
   'farmergreg/vim-lastplace',
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
   'rust-lang/rust.vim',
-
+  defaults = {
+    auto_update = false, -- Disable automatic updates
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -755,7 +758,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'catppuccin-mocha'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -810,6 +814,7 @@ require('lazy').setup({
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
+      auto_update = false, -- Disable updates for treesitter
       auto_install = true,
       highlight = {
         enable = true,
